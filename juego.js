e880e752a1008eea4f9a8a1748aa9eb26c2517b4
AldoYamil <119721837+AldoYamil@users.noreply.github.com>
2022-12-03 11:13:57 -0800

    Update juego.js

--- juego.js
+++ juego.js
@@ -1,19 +1,26 @@
-var score=0;
+var score=0;//esta linea es una variable que se va a encargar de cargar los puntos
+//esta linea es un ciclo for donde indico que quiero que se repita hasta que gane 3 puntos
 for(i=1; score<=3; score++){
-alert("Elige");
-playerone = prompt("tijeras:0\npapel:1\npiedra:2");
-function pcopcion(minimo, maximo){
+alert("Elige"); //esta linea me presenta el primer mensaje donde me dice que eliga un numero para empezar a jugar
+playerone = prompt("tijeras:0\npapel:1\npiedra:2");   //esta linea pu esta pidiendo que ingrese un numero para jugar
+function pcopcion(minimo, maximo){ //esta funcion genera un numero aleatorio entre el o y el 2 para que la computadora pueda jugar
 var numero = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 return numero;
 }
+    // las siguientes 3 lineas son variables en las que declaro la opciones de juego y le asigno un valor a cada una para poder elegir
 var tijeras = 0;
 var papel = 1;
 var piedra = 2;
+    //esla linea es una constante que me da opciones y que nunca van a cambiar en este juego
 const opciones=["tijeras", "papel", "piedra"];
+    //estas variables son donde declaro como voy a jugar y como va a jugar la computadora
 var playerone;
 var computadora = pcopcion(0,2);
+    //las dos lineas sigientes me mostraran un mensaje en el que me diran que opcion escogi para jugar y que opcion escogio la computadora para jugar
 alert("playerone escogio"+ opciones[playerone]);
 alert("computadora escogio"+ opciones[computadora]);
+    //de la linea 22 hasta la 61 es todo un gran if donde se muestra lo que quiero que pase en caso de que yo escoga cualquier opcion y la computadora escoga otra y si gano me suma un punto y si escogo un numero que no esta
+    //entoces me marca el mensage mal elegido.
 if(playerone == tijeras){
     if(computadora == tijeras){
         alert("Empataron")
@@ -52,4 +59,5 @@ else if(playerone == piedra){
 }
 else{
     alert("MAL ELEGIDO")}
+    //esta linea me aparecera un mensaje donde se estaran contando los puntos y cada vez que gane 3 puntos el juego terminara
 alert("player one gano " + score + " partidas y computadora gano " + score + " partidas")}
