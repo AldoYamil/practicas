34079a22eef683b3cddbfe37ca29d8d33aea1fbb
AldoYamil <119721837+AldoYamil@users.noreply.github.com>
2022-12-03 11:57:02 -0800

    Update a11ArrowFuntion.js

--- a11ArrowFuntion.js
+++ a11ArrowFuntion.js
@@ -1,3 +1,4 @@
+//aqui estoy creando un objeto donde muestra los datos de sacha
 var sacha ={
     nombre: "Sacha",
     apellido: "Smith",
@@ -13,12 +14,12 @@ var sacha ={
 //const MAYORIA_DE_EDAD = 17;
 
 
-
+//aqui estoy creando una funcion en la que le dijo que si la persona es mayor de edad me de acceso 
 function permitirAcceso (persona){
    if (!esMayorDeEdad(persona)){
-       console.log(`acceso denegado`);}
+       console.log(`acceso consedido`);}
    }
-
+//aqui estoy creando una funcion en la que le dijo que si la persona no es mayor de edad no me de acceso
    const accesoEdad = persona => persona.edad; 
    function permitirAcceso(persona) { 
     if(accesoEdad (persona)) { 
